0a636621ac9e6d759052ed828fb5c29026df9d4e
Tucanu <rockerulmaniac454@gmail.com>
2025-01-30 04:15:25 +0200

    Fixed some display issues

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -5,7 +5,7 @@ export default function Team() {
     { name: "Pietriș Dan", role: "Mentor", image: "", description: "A dedicated mentor who always inspires innovation and teamwork, guiding the team through challenges." },
     { name: "Sorin", role: "Team Leader", image: "/images/absent.png", description: "The visionary Team Leader, orchestrating every project with strategic thinking and unparalleled leadership." },
     { name: "Țucă", role: "Programator", image: "/images/tuca.png", description: "A talented programmer, constantly pushing the limits of coding to create seamless user experiences." },
-    { name: "Luca", role: "Designer 3D", image: "/images/luca.luca", description: "A creative mind in 3D design, bringing visual concepts to life with passion and precision." },
+    { name: "Luca", role: "Designer 3D", image: "/images/luca.png", description: "A creative mind in 3D design, bringing visual concepts to life with passion and precision." },
     { name: "Măldă", role: "Marketing", image: "/images/coma.png", description: "The marketing genius, making sure our team's work reaches the right audience with impactful campaigns." },
     { name: "Radu", role: "Marketing", image: "/images/malda.png", description: "A creative force in marketing, always brainstorming fresh ideas to keep the team ahead of trends." },
     { name: "Cătălin", role: "Mecanic", image: "/images/samanta.png", description: "The mechanic behind the magic, ensuring everything functions perfectly with expert hands-on knowledge." },
